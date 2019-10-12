f6c15ce076d92666aaae5e1cab83bd16ec5a1d9c
Kirill Korniakov <kirill.korniakov@intel.com>
2019-10-12 12:43:13 +0300

    Added fake returns to avoid compile-time warnings

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -7,6 +7,10 @@
 
 #include "tbitfield.h"
 
+// Fake variables used as placeholders in tests
+static const int FAKE_INT = -1;
+static TBitField FAKE_BITFIELD(1);
+
 TBitField::TBitField(int len)
 {
 }
@@ -21,17 +25,19 @@ TBitField::~TBitField()
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+    return FAKE_INT;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+    return FAKE_INT;
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+  return FAKE_INT;
 }
 
 void TBitField::SetBit(const int n) // установить бит
@@ -44,43 +50,49 @@ void TBitField::ClrBit(const int n) // очистить бит
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+  return FAKE_INT;
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+    return FAKE_BITFIELD;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return 0;
+  return FAKE_INT;
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение
 {
-  return 0;
+  return FAKE_INT;
 }
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
+    return FAKE_BITFIELD;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
+    return FAKE_BITFIELD;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
+    return FAKE_BITFIELD;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+    return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+    return ostr;
 }
--- src/tset.cpp
+++ src/tset.cpp
@@ -7,6 +7,11 @@
 
 #include "tset.h"
 
+// Fake variables used as placeholders in tests
+static const int FAKE_INT = -1;
+static TBitField FAKE_BITFIELD(1);
+static TSet FAKE_SET(1);
+
 TSet::TSet(int mp) : BitField(-1)
 {
 }
@@ -23,15 +28,17 @@ TSet::TSet(const TBitField &bf) : BitField(-1)
 
 TSet::operator TBitField()
 {
+    return FAKE_BITFIELD;
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
+    return FAKE_INT;
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
 {
-    return 0;
+    return FAKE_INT;
 }
 
 void TSet::InsElem(const int Elem) // включение элемента множества
@@ -46,43 +53,52 @@ void TSet::DelElem(const int Elem) // исключение элемента мн
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
+    return FAKE_SET;
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
 {
-    return 0;
+    return FAKE_INT;
 }
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
+    return FAKE_INT;
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
+    return FAKE_SET;
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
+    return FAKE_SET;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
+    return FAKE_SET;
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
+    return FAKE_SET;
 }
 
 TSet TSet::operator~(void) // дополнение
 {
+    return FAKE_SET;
 }
 
 // перегрузка ввода/вывода
 
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
+    return istr;
 }
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
+    return ostr;
 }
